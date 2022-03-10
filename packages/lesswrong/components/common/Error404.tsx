882005a857394adc0b7b73ea7e6897f7e6eb0570
Sameer Ismail <>
2022-03-10 23:36:16 +1000

    Add default font to 404

--- packages/lesswrong/components/common/Error404.tsx
+++ packages/lesswrong/components/common/Error404.tsx
@@ -2,20 +2,30 @@ import { Components, registerComponent } from '../../lib/vulcan-lib';
 import React from 'react';
 import { useServerRequestStatus } from '../../lib/routeUtil'
 
-const Error404 = () => {
+const styles = (theme: ThemeType): JssStyles => ({
+  root: {
+    ...theme.typography.postStyle,
+  }
+});
+
+const Error404 = ({ classes }: {
+  classes: ClassesType,
+}) => {
   const { SingleColumnSection } = Components;
   const serverRequestStatus = useServerRequestStatus()
   if (serverRequestStatus) serverRequestStatus.status = 404
   
   return (
-    <SingleColumnSection>
-      <h2>404 Not Found</h2>
-      <h3>Sorry, we couldn't find what you were looking for.</h3>
-    </SingleColumnSection>
+    <div className={classes.root}>
+      <SingleColumnSection>
+          <h2>404 Not Found</h2>
+          <h3>Sorry, we couldn't find what you were looking for.</h3>
+      </SingleColumnSection>
+    </div>
   );
 };
 
-const Error404Component = registerComponent('Error404', Error404);
+const Error404Component = registerComponent('Error404', Error404, { styles });
 
 declare global {
   interface ComponentTypes {
