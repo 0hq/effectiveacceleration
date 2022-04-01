3b1e823f772db2e0631784e3c8ef8bf9118966da
Sameer Ismail <>
2022-04-02 00:01:31 +1000

    Hide Y-axis overflow in UsersName component

--- packages/lesswrong/components/posts/PostsUserAndCoauthors.tsx
+++ packages/lesswrong/components/posts/PostsUserAndCoauthors.tsx
@@ -9,6 +9,7 @@ const styles = (theme: ThemeType): JssStyles => ({
     maxWidth: 310,
     textOverflow: "ellipsis",
     overflowX: "hidden",
+    overflowY: "hidden",
     textAlign: "right",
     [theme.breakpoints.down('xs')]: {
       maxWidth: 160
