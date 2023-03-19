f61267c619ab8a4a95335feee295e810a9fe373d
Will DePue <will@depue.net>
2023-03-18 18:51:12 -0700

    Fixed opengraph

--- packages/lesswrong/components/common/HeadTags.tsx
+++ packages/lesswrong/components/common/HeadTags.tsx
@@ -1,80 +1,89 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import { Components, registerComponent } from '../../lib/vulcan-lib';
-import { combineUrls, getBasePath, getSiteUrl } from '../../lib/vulcan-lib/utils';
-import { useSubscribedLocation } from '../../lib/routeUtil';
-import { PublicInstanceSetting } from '../../lib/instanceSettings';
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Components, registerComponent } from "../../lib/vulcan-lib";
+import { combineUrls, getBasePath, getSiteUrl } from "../../lib/vulcan-lib/utils";
+import { useSubscribedLocation } from "../../lib/routeUtil";
+import { PublicInstanceSetting } from "../../lib/instanceSettings";
 
-export const taglineSetting = new PublicInstanceSetting<string>('tagline', "A community blog devoted to refining the art of rationality", "warning")
-export const faviconUrlSetting = new PublicInstanceSetting<string>('faviconUrl', '/img/favicon.ico', "warning")
-const tabTitleSetting = new PublicInstanceSetting<string>('forumSettings.tabTitle', 'LessWrong', "warning")
+export const taglineSetting = new PublicInstanceSetting<string>(
+  "tagline",
+  "A community blog devoted to refining the art of rationality",
+  "warning"
+);
+export const faviconUrlSetting = new PublicInstanceSetting<string>("faviconUrl", "/img/favicon.ico", "warning");
+const tabTitleSetting = new PublicInstanceSetting<string>("forumSettings.tabTitle", "LessWrong", "warning");
 
-
-const HeadTags = ({ogUrl: ogUrlProp, canonicalUrl: canonicalUrlProp, description: descriptionProp, title: titleProp, image, noIndex}: {
-  ogUrl?: string,
-  canonicalUrl?: string,
-  description?: string|null,
-  title?: string,
-  image?: string|null,
-  noIndex?: boolean,
+const HeadTags = ({
+  ogUrl: ogUrlProp,
+  canonicalUrl: canonicalUrlProp,
+  description: descriptionProp,
+  title: titleProp,
+  image,
+  noIndex,
+}: {
+  ogUrl?: string;
+  canonicalUrl?: string;
+  description?: string | null;
+  title?: string;
+  image?: string | null;
+  noIndex?: boolean;
 }) => {
-    const { currentRoute, pathname } = useSubscribedLocation();
-    // The default url we want to use for our cannonical and og:url tags uses
-    // the "base" path, site url and path without query or hash
-    const url = combineUrls(getSiteUrl(), getBasePath(pathname))
-    const ogUrl = ogUrlProp || url
-    const canonicalUrl = canonicalUrlProp || url
-    const description = descriptionProp || taglineSetting.get()
-    const siteName = tabTitleSetting.get()
-    
-    const TitleComponent: any = currentRoute?.titleComponentName ? Components[currentRoute.titleComponentName] : null;
-    const titleString = currentRoute?.title || titleProp || currentRoute?.subtitle;
-    
-    const rssUrl = `${getSiteUrl()}feed.xml`
-    
-    return (
-      <React.Fragment>
-        { TitleComponent
-            ? <TitleComponent siteName={siteName} isSubtitle={false} />
-            : <Helmet><title>
-                {titleString
-                  ? `${titleString} - ${siteName}`
-                  : siteName}
-              </title></Helmet>
-        }
+  const { currentRoute, pathname } = useSubscribedLocation();
+  // The default url we want to use for our cannonical and og:url tags uses
+  // the "base" path, site url and path without query or hash
+  const url = combineUrls(getSiteUrl(), getBasePath(pathname));
+  const ogUrl = ogUrlProp || url;
+  const canonicalUrl = canonicalUrlProp || url;
+  const description = descriptionProp || taglineSetting.get();
+  const siteName = tabTitleSetting.get();
 
-        <Helmet key={pathname}>
-          <meta charSet='utf-8'/>
-          <meta name='description' content={description}/>
-          <meta name='viewport' content='width=device-width, initial-scale=1'/>
+  const TitleComponent: any = currentRoute?.titleComponentName ? Components[currentRoute.titleComponentName] : null;
+  const titleString = currentRoute?.title || titleProp || currentRoute?.subtitle;
 
-          {/* facebook */}
-          <meta property='og:type' content='article'/>
-          <meta property='og:url' content={ogUrl}/>
-          {image && <meta property='og:image' content={image}/>}
-          { /* <meta property='og:title' content={title}/> */ }
-          <meta property='og:description' content={description}/>
+  const rssUrl = `${getSiteUrl()}feed.xml`;
 
-          {/* twitter */}
-          <meta name='twitter:card' content='summary'/>
-          {image && <meta name='twitter:image:src' content={image}/>}
-          { /* <meta name='twitter:title' content={title}/> */ }
-          <meta name='twitter:description' content={description}/>
-
-          {(noIndex || currentRoute?.noIndex) && <meta name='robots' content='noindex' />}
-          <link rel='canonical' href={canonicalUrl}/>
-          <link rel="shortcut icon" sizes="192x192" href={faviconUrlSetting.get()} />
-
-          <link rel="alternate" type="application/rss+xml" href={rssUrl} />
+  return (
+    <React.Fragment>
+      {TitleComponent ? (
+        <TitleComponent siteName={siteName} isSubtitle={false} />
+      ) : (
+        <Helmet>
+          <title>{titleString ? `${titleString} - ${siteName}` : siteName}</title>
         </Helmet>
-      </React.Fragment>
-    );
-}
+      )}
 
-const HeadTagsComponent = registerComponent('HeadTags', HeadTags);
+      <Helmet key={pathname}>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* facebook */}
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content={ogUrl} />
+        {image && <meta property="og:image" content={image} />}
+        <meta property="og:title" content={titleString} />
+        <meta property="og:description" content={description} />
+
+        {/* twitter */}
+        <meta name="twitter:card" content="summary" />
+        {image && <meta name="twitter:image:src" content={image} />}
+        <meta name="twitter:title" content={titleString} />
+        <meta name="twitter:description" content={description} />
+
+        {(noIndex || currentRoute?.noIndex) && <meta name="robots" content="noindex" />}
+        <link rel="canonical" href={canonicalUrl} />
+        <link rel="shortcut icon" sizes="192x192" href={faviconUrlSetting.get()} />
+
+        <link rel="alternate" type="application/rss+xml" href={rssUrl} />
+      </Helmet>
+    </React.Fragment>
+  );
+};
+
+const HeadTagsComponent = registerComponent("HeadTags", HeadTags);
 
 declare global {
   interface ComponentTypes {
-    HeadTags: typeof HeadTagsComponent
+    HeadTags: typeof HeadTagsComponent;
   }
 }
